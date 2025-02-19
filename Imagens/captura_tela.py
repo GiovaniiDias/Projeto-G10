3efd457b6db86a0151ce00d32563d64090645754
Giovani Dias <giovani.eb3@gmail.com>
2025-02-19 10:16:37 -0300

    versao2

--- Imagens/captura_tela.py
+++ Imagens/captura_tela.py
@@ -6,4 +6,5 @@ pyautogui.screenshot('tela.jpg')
 
 #tirar print pontos especificos
 pyautogui.screenshot('recorte.jpg', region=(904,80,611,715))
-# sempre colocar o nome e a extenção. a região de corte (ponto de partida'904,80', para direita'611', para baixo'715')
+# sempre colocar o nome e a extenção. a região de corte (ponto de partida'904,80', para direita'611', para baixo'715')   
+  
