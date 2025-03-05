14da112437b96d7c5bfd9b3fb5fa75c554ca259e
Giovani Dias <giovani.eb3@gmail.com>
2025-03-05 18:41:40 -0300

    novo scrapy

--- Projetos Scrapy/varredor_de_sites/varredor_de_sites/items.py
+++ Projetos Scrapy/varredor_de_sites/varredor_de_sites/items.py
@@ -4,9 +4,30 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst, Join
 
 
-class VarredorDeSitesItem(scrapy.Item):
+def tira_espaços_em_branco(valor):
+    return valor.strip()
+
+def processar_caracteres_especiais(valor):
+    return valor.replace(u"201c",'').replaceu(u"\u201d",'')
+   
+
+
+class CitacaoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    frase = scrapy.Field(
+        input_processor=MapCompose(tira_espaços_em_branco, processar_caracteres_especiais ),
+        output_processor=TakeFirst()
+
+    )
+    autor = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    tags = scrapy.Field(
+        output_processor=Join(',')
+    )
+
     pass
