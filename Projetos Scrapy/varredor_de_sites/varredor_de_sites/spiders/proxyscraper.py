fb2dcab3f77d5729c824d07bd289eb1ede0a46c6
Giovani Dias <giovani.eb3@gmail.com>
2025-03-06 15:51:01 -0300

    Varrendo sites com link

--- Projetos Scrapy/varredor_de_sites/varredor_de_sites/spiders/proxyscraper.py
+++ Projetos Scrapy/varredor_de_sites/varredor_de_sites/spiders/proxyscraper.py
@@ -6,7 +6,7 @@ class ProxyScraperSpider(scrapy.Spider):
     name = 'proxyscraper'
     # request
     def start_requests(self):
-        urls = ['https://us-proxy.org/']
+        urls = ['https://free-proxy-list.net/web-proxy.html']
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
 
@@ -14,12 +14,10 @@ class ProxyScraperSpider(scrapy.Spider):
     def parse(self, response):
         for linha in response.xpath("//table[@class='table table-striped table-bordered']//tr"):
             yield {
-                'IP Address': linha.xpath('./td[1]/text()').get(),
-                'Port': linha.xpath('./td[2]/text()').get(),
-                'Code': linha.xpath('./td[3]/text()').get(),
-                'Country': linha.xpath('./td[4]/text()').get(),
-                'Anonimal': linha.xpath('./td[5]/text()').get(),
-                'Google': linha.xpath('./td[6]/text()').get(),
-                'Https': linha.xpath('./td[7]/text()').get(),
-                'Last Checked': linha.xpath('./td[8]/text()').get()
+                'Proxy Name': linha.xpath('./td[1]/a/text()').get(),
+                'Domain': linha.xpath('./td[2]/text()').get(),
+                'Contry': linha.xpath('./td[3]/text()').get(),
+                'Speed': linha.xpath('./td[4]/text()').get(),
+                'Pop': linha.xpath('./td[5]/div/div/text()').get()
+                
             }
