54e33fded2553f5416fb8dea69bd7f583cbac9f4
Giovani Dias <giovani.eb3@gmail.com>
2025-03-22 12:51:36 -0300

    Criando planilhas com python

---
+++ Projetos Planilhas/Desafio Planilhas/criando_planilhas.py
@@ -0,0 +1,28 @@
+import openpyxl
+import openpyxl.workbook
+
+worbook = openpyxl.Workbook()
+
+# deletando a sheet padrao
+del worbook['Sheet']
+
+# nome da planilha
+#worbook.save('computadores.xlsx')
+
+#Criando a sheet 1
+worbook.create_sheet('Produtos')
+
+produtos = worbook['Produtos']
+
+# cabeçalho
+produtos.append(['computador', 'ano','preço'])
+worbook.save('computadores.xlsx')
+
+# adicionando linhas e dados nas linhas
+produtos.append(['Computador 1', '2001' , 500])
+produtos.append(['Computador 2', '2002' , 1000])
+produtos.append(['Computador 3', '2003' , 1500])
+produtos.append(['Computador 4', '2004' , 2500])
+produtos.append(['Computador 5', '2005' , 3000])
+worbook.save('computadores.xlsx')
+
--- Projetos Planilhas/base_planilhas.py
+++ Projetos Planilhas/base_planilhas.py
@@ -30,3 +30,23 @@ del workbook['Sheet']
 
 print(workbook.sheetnames)
 workbook.save('endereços.xlsx')
+
+
+# colocaando cabeçalho
+# criando uma variavel com o nome da sheet, para facilitar a chamada
+sheet_planilha01 = workbook['Planilha 01']
+#cabeçlho
+sheet_planilha01.append(['rua','cep','casa'])
+workbook.save('endereços.xlsx')
+
+# adicionar dados na linha
+sheet_planilha01.append(['endereço 1','numero cep 1', 'casa 1'])
+sheet_planilha01.append(['endereço 2','numero cep 2', 'casa 2'])
+sheet_planilha01.append(['endereço 3','numero cep 3', 'casa 3'])
+sheet_planilha01.append(['endereço 4','numero cep 4', 'casa 4'])
+workbook.save('endereços.xlsx')
+
+# adicionando dados pelo endereço da cedula
+sheet_planilha01['A6'].value = 'endereço 5'
+sheet_planilha01['B6'].value = 'numero cep 5'
+sheet_planilha01['C6'].value = 'casa 5'
